4af22a7169a6d71f8055ce9587bc1a293c14cd72
Damir <pt7mk@yandex.ru>
2019-01-25 21:19:47 +0700

    build project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForJenkins
+{
+    class Program
+    {
+        private static void Test(int a)
+        {
+            for (var i = 0; i < a; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Test(a);
+            Console.Read();
+        }
+    }
+}
